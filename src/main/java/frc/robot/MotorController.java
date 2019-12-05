77cbef431970951ff9c8afd8633c33b4415c3e73
LeagueOfFreeWorlds <inathan2347@gmail.com>
2019-12-05 16:55:11 -0700

    PUSH

--- src/main/java/frc/robot/MotorController.java
+++ src/main/java/frc/robot/MotorController.java
@@ -9,10 +9,27 @@ package frc.robot;
 
 /// BEGIN LIBRARIES ///
 import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
+import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.VictorSP;
+import com.ctre.phoenix.motorcontrol.Faults;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+
 /**
  * Add your docs here.
  */
 public class MotorController {
-    TaloxSRX  
+    TalonSRX motor1 = new TaloxSRX(1);
+    TalonSRX motor2 = new TalonSRX(deviceNumber);
+    // EEEE
+
+
 
 }
