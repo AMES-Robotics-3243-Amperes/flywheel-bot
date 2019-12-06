76c4bc6afea45fc9ee6be690cb9bfc0697fc9d16
LeagueOfFreeWorlds <inathan2347@gmail.com>
2019-12-05 18:30:34 -0700

    Added DriveTrain

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -4,20 +4,11 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-// MotorController will be used for operating the flywheel CIM and servo
+
 package frc.robot;
-
-/// BEGIN LIBRARIES ///
-import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
-
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
 
 /**
  * Add your docs here.
  */
-public class MotorController {
-    TalonSRX motor1 = new TalonSRX(1);
-
+public class DriveTrain {
 }
