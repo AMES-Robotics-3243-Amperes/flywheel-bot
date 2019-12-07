5d9f4c89167d017851ae53cf4ff32a6f5b5002e5
LeagueOfFreeWorlds <inathan2347@gmail.com>
2019-12-07 12:30:20 -0700

    Deleted DriveTrain, since it will be incorporated into the MotorController class. Considering adding ArrayList; it may help us. Added two new methods to MotorController.

--- src/main/java/frc/robot/MotorController.java
+++ src/main/java/frc/robot/MotorController.java
@@ -8,27 +8,33 @@
 package frc.robot;
 
 /// BEGIN LIBRARIES ///
-import com.ctre.phoenix.motorcontrol.can.TalonSRX; 
-
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-
-import edu.wpi.first.wpilibj.Talon;
 import com.ctre.phoenix.motorcontrol.can.*;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.VictorSP; // Victor motorcont libraries
+import edu.wpi.first.wpilibj.Joystick; // Wpilibj generic joystick library
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Talon; // Talon motorcont libraries
+import edu.wpi.first.wpilibj.Servo; // Generic servo library
+import java.util.ArrayList; // Should we use an arraylist? Would it make life easier?
 
 public class MotorController 
 {
 	WPI_TalonSRX driveM1 = new WPI_TalonSRX(0);
     WPI_TalonSRX driveM2 = new WPI_TalonSRX(1);
-	VictorSP driveM3 = new VictorSP(0);
-	VictorSP driveM4 = new VictorSP(1);
+	WPI_TalonSRX driveM3 = new WPI_TalonSRX(2);
+    WPI_TalonSRX driveM4 = new WPI_TalonSRX(3);
+    VictorSPX flywheel = new VictorSPX(0);
+
 	
 	public void drive(Double [] val) {
 		driveM1.set(-val[0]); // Inverted motor 1
 		driveM2.set(val[1]);
 		driveM3.set(-val[0]); // Inverted motor 2
 		driveM4.set(val[1]);
-	}
+    }
+    public void flywheelMotor(boolean [] val){ // Boolean method for CIM motor on flywheel mech
+        
+    }
+    public void servoControl(int [] val){
+
+    }
 }
